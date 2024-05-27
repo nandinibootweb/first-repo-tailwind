c1abe5dff2f16b374a2052e9c32347a4184b05ae
Felipe Borja <165383150+felborja@users.noreply.github.com>
2024-05-27 16:58:39 +0200

    Our Popular Section for Desktop done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        lato: ["Lato"],
+        garamond: ["Garamond"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
+};
