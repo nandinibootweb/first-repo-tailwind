ce835ca768484e481c20c8a3ec9db6102795903f
Felipe Borja <165383150+felborja@users.noreply.github.com>
2024-05-30 07:44:50 +0200

    New update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,3 +11,15 @@ module.exports = {
   variants: {},
   plugins: [require("@tailwindcss/ui")],
 };
+
+module.exports = {
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+};
